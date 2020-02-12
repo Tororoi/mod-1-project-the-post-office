7afa0b056496fedfc4731c1e423c81698725a399
Tororoi <cantwell.tom@gmail.com>
2020-02-11 21:59:10 -0500

    Added address book and reduced possibility for user error when entering names

--- app/models/interface.rb
+++ app/models/interface.rb
@@ -41,22 +41,24 @@ class Interface
 
         def login 
             puts "Please enter your name:"
-            @sender_input = gets.chomp.titlecase
+            @sender_input = gets.chomp.titlecase.strip
             if valid_user
                 puts "Welcome Back, #{@sender_input}"
             else
                 @sender = Sender.create(name: @sender_input)
                 puts "Please enter your address:"
-                @sender_address_input = gets.chomp.titlecase
+                @sender_address_input = gets.chomp.titlecase.strip
                 sender.address = @sender_address_input
             end
         end
 
         def menu
+            system "clear"
             option = prompt.select("What would you like to do?") do |menu|
                 menu.choice 'Send a letter'
                 menu.choice 'View letters' #disabled?: sender.letters.length
                 menu.choice 'Change address'
+                menu.choice 'View Address Book'
                 menu.choice "Exit 'The Post Office'"
             end
             if option == 'Send a letter'
@@ -68,18 +70,22 @@ class Interface
             elsif option == 'Change address'
                 system "clear"
                 info_change(@sender)
-            elsif option == 'Exit Menu'
+            elsif option == 'View Address Book'
                 system "clear"
+                address_book
+            elsif option == "Exit 'The Post Office'"
+                system "clear"
+                exit
             end
         end
 
         def letter(sender)
             puts("Who would like to write to?")
-            receiver_name = gets.chomp.titlecase
+            receiver_name = gets.chomp.titlecase.strip
             receiver_inst = Receiver.find_by(name: receiver_name)
             if receiver_inst == nil
-                puts "#{receiver_name.capitalize} is not in your address book. Please enter their address:"
-                receiver_address = gets.chomp
+                puts "#{receiver_name} is not in your address book. Please enter their address:"
+                receiver_address = gets.chomp.strip
                 receiver_inst = Receiver.create(name: receiver_name, address: receiver_address)
             end
             puts "Write your letter below"
@@ -91,15 +97,18 @@ class Interface
             menu
         end
 
-        def view_outbox(sender)
+        def view_outbox(sender) ## returns error if empty
             outbox = Letter.all.select do |letter|
                 letter.sender_id == sender.id
             end
             choices = outbox.map do |letter|
                 letter.receiver.name
             end.uniq
+            choices << "Return to Main Menu"
             sent_to = prompt.select("Please choose a receiver:", choices)
-            if sent_to 
+            if sent_to == "Return to Main Menu"
+                menu
+            else
                 system "clear"
                 friend = Receiver.find_by(name: sent_to)
                 mail = friend.letters.map do |letter|
@@ -115,16 +124,19 @@ class Interface
                     if delete_letter == "Delete Letter"
                         mail_delete(view_mail)
                     else
+                        menu
                     end
                 end
             end
             
-            yes_no = prompt.select("Are you ready to return to the Main Menu?") do |menu|
-                menu.choice "Yes"
-                # menu.choice "No"
+            main_menu
+        end
+
+        def main_menu 
+            prompt.select("Are you ready to return to the Main Menu?") do |menu|
+            menu.choice "Yes"
+            # menu.choice "No"
             end
-            sleep(0.5)
-            system "clear"
             menu
         end
 
@@ -136,12 +148,20 @@ class Interface
         def info_change(sender)
             puts "Your current address is #{sender.address}"
             puts "What would you like to change your address to?"
-            new_address = gets.chomp.titlecase
+            new_address = gets.chomp.titlecase.strip
             sender.update(address: new_address)
             puts "Your new address is now: #{sender.address}"
             sleep(2.0)
             system "clear"
             menu
         end
-    
+
+        def address_book
+            Receiver.all.each do |person|
+                puts "Name: #{person.name}"
+                puts "Address: #{person.address}"
+                puts ""
+            end
+            main_menu
+        end
 end
